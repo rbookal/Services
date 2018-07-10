0030292e2fb3a9532724a1be84fcea498228b855
rbookal <rbookal@yahoo.com>
2018-07-10 09:07:21 -0400

    Add files via upload

---
+++ CheckSNMPService.ps1
@@ -0,0 +1,34 @@
+﻿$workstation = Get-Content -Path 'c:\temp\file.txt'
+
+function FuncCheckService
+{
+param($ServiceName)
+
+ 
+ foreach ($computer in $workstation)
+   {
+    $arrService = Get-Service -Name $ServiceName -ComputerName $computer -ErrorAction Ignore
+
+    if ($arrService.Status -ne "Running")
+     {
+     #Start-Service $ServiceName
+     #Write-Host $arrService.Name "Not Started  service" 
+    $computer| Out-File C:\temp\NotStarted.txt -Append
+     #Write-Host $_
+     }
+    elseif ($arrService.Status -eq "running")
+     { 
+     #Start-Service $ServiceName
+     #Write-Host "$ServiceName service is already started"
+     $computer | Out-File c:\temp\cupstarted.txt -Append
+    #Write-Host  $arrService +  " running"
+     }
+       
+   }
+
+
+   
+ }
+
+ FuncCheckService -ServiceName 'snmp'
+
---
+++ RestartServiceFromList.ps1
@@ -0,0 +1,26 @@
+﻿Set-ExecutionPolicy -ExecutionPolicy Bypass -Force
+
+$workstation = Get-Content -Path C:\RebootShutdownList.txt
+
+
+$computer = Get-Service -DisplayName ServiceName-ComputerName $workstation
+
+Restart-Service -DisplayName ServiceName- -Force
+
+
+function FuncCheckService{param($ServiceName)
+
+ $arrService = Get-Service -Name $ServiceName
+ if ($arrService.Status -ne "Running")
+ {
+ Start-Service $ServiceName
+ Write-Host "Starting " $ServiceName " service" 
+ " ---------------------- " 
+ " Service is now started"
+ }
+ if ($arrService.Status -eq "running")
+ { 
+ Start-Service $ServiceName
+ Write-Host "$ServiceName service is already started"
+ }
+ }
